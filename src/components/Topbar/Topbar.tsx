a81cd7313bc71f16d7076163d2e03eedceaa0355
corlard3y <corlardey@gmail.com>
2025-07-14 22:50:32 +0100

    chore: add topbar

---
+++ src/components/Topbar/Topbar.tsx
@@ -0,0 +1,36 @@
+import { FiSearch } from 'react-icons/fi';
+import { BsBell, BsFillCaretDownFill } from "react-icons/bs";
+
+import LendsqrLogo from '../../assets/illustrations/LendsqrLogo.svg'
+
+import './Topbar.scss';
+
+
+export const Topbar = () => {
+  return (
+    <div className="topbar">
+        <img src={LendsqrLogo} className='topbar-lopo' alt="Lendsqr Logo" />
+
+      <div className="search-bar">
+        <input type="text" placeholder="Search for anything" />
+        <button type="button">
+          <FiSearch size={18} />
+        </button>
+      </div>
+
+
+      <div className="user-info">
+        <a href="#">Docs</a>
+
+        <BsBell className='bell-icon' />
+
+        <div style={{ width: '45px', height: '45px', backgroundColor: 'red', borderRadius: '100%' }}></div>
+
+        <div className='user-name'>
+          <p>Adedeji</p>
+          <BsFillCaretDownFill />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/structure/MainLayout.tsx
+++ src/structure/MainLayout.tsx
@@ -1,12 +1,19 @@
 import { Outlet } from 'react-router-dom';
+import { Topbar } from '../components/Topbar/Topbar';
+import './MainLayout.scss';
 
 const MainLayout = () => {
   return (
     <div className="main-layout">
+      <div className='topbar-section'>
+        <Topbar />
+      </div>
+      <div>
       <aside>Sidebar</aside>
       <main>
         <Outlet />
       </main>
+      </div>
     </div>
   );
 };
