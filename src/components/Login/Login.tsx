75b3cc8f9c37e8d5bcce2917d647a49b3cb43b21
corlard3y <corlardey@gmail.com>
2025-07-14 17:12:40 +0100

    add media breakpoint && login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,28 +1,32 @@
 import LendsqrLogo from '../../assets/illustrations/LendsqrLogo.svg'
+import LendsqrLoginImage from '../../assets/illustrations/LendsqrLoginImage.webp'
 import './Login.scss'
 
 export default function LoginComponent() {
   return (
     <div className='login-wrapper'>
       <div className='image-wrapper'>
-        <img src={LendsqrLogo} alt="Lendsqr Logo" />
+        <img src={LendsqrLogo} className='login-logo' alt="Lendsqr Logo" />
+        <img src={LendsqrLoginImage} className='login-image' alt='Lendsqr Login Image' />
       </div>
+
       <div className="form-wrapper">
         <div>
-          <h2>Welcome</h2>
+          <h2>Welcome!</h2>
           <span>Enter details to login.</span>
         </div>
-        <div className="form">
+
+        <form className="form">
           <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Enter your email" />
+            <input type="email" id="email" placeholder="Email" />
           </div>
           <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="Enter your password" />
+            <input type="password" id="password" placeholder="Password" />
           </div>
-          <button type="submit">Login</button>
-        </div>
+
+          <p>Forgot PASSWORD?</p>
+          <button type="submit">Log in</button>
+        </form>
       </div>
     </div>
   )
