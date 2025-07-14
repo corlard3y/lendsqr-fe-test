c5c2c99f91783434a03033f4e2e5af0c3e444eb4
corlard3y <corlardey@gmail.com>
2025-07-14 17:33:19 +0100

    add formik and validate form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,13 +1,31 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 import LendsqrLogo from '../../assets/illustrations/LendsqrLogo.svg'
 import LendsqrLoginImage from '../../assets/illustrations/LendsqrLoginImage.webp'
 import './Login.scss'
 
+const schema = Yup.object({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('Required'),
+})
+
 export default function LoginComponent() {
+  const handleSubmit = async (values: { email: string; password: string }) => {
+    // const res = await fetch('/api/login', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify(values),
+    // })
+    // const data = await res.json()
+    // console.log(data)
+    console.log(values, 'values')
+  }
+
   return (
     <div className='login-wrapper'>
       <div className='image-wrapper'>
         <img src={LendsqrLogo} className='login-logo' alt="Lendsqr Logo" />
-        <img src={LendsqrLoginImage} className='login-image' alt='Lendsqr Login Image' />
+        <img src={LendsqrLoginImage} className='login-image' alt='Lendsqr Login' />
       </div>
 
       <div className="form-wrapper">
@@ -16,17 +34,26 @@ export default function LoginComponent() {
           <span>Enter details to login.</span>
         </div>
 
-        <form className="form">
-          <div className="input-group">
-            <input type="email" id="email" placeholder="Email" />
-          </div>
-          <div className="input-group">
-            <input type="password" id="password" placeholder="Password" />
-          </div>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={schema}
+          onSubmit={handleSubmit}
+        >
+          <Form className="form">
+            <div className="input-group">
+              <Field type="email" name="email" placeholder="Email" />
+              <ErrorMessage name="email" component="small" className="error" className='error' />
+            </div>
 
-          <p>Forgot PASSWORD?</p>
-          <button type="submit">Log in</button>
-        </form>
+            <div className="input-group">
+              <Field type="password" name="password" placeholder="Password" />
+              <ErrorMessage name="password" component="small" className="error" className='error' />
+            </div>
+
+            <p>Forgot PASSWORD?</p>
+            <button type="submit">Log in</button>
+          </Form>
+        </Formik>
       </div>
     </div>
   )
